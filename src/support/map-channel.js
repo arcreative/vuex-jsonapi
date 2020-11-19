99e69a428fba1c1cad3f3600f83d27c0d05c9587
Adam Robertson <adam@arcreative.net>
2020-11-18 16:21:44 -0800

    Add dynamic option to mapChannels
    Adding dynamic option to signify that the channel name should be pulled from a property instead of the defined channel name.  Helps if you don't know what the channel name is ahead of time, e.g. when you have a component that could be instantiated in multiple places, but shouldn't clobber data for unrelated views

--- src/support/map-channel.js
+++ src/support/map-channel.js
@@ -1,10 +1,22 @@
-export default (channel, name = null) => {
+export default (channel, name = null, { dynamic = false } = {}) => {
   return {
-    [name ? name : 'model']() { return this.$store.getters.channel(channel) },
-    [name ? name + 'Error' : 'error']() { return this.$store.getters.error(channel) },
-    [name ? name + 'Loading' : 'loading']() { return this.$store.getters.loading(channel) },
-    [name ? name + 'Meta' : 'meta']() { return this.$store.getters.meta(channel) },
-    [name ? name + 'MoreRecords' : 'moreRecords']() { return this.$store.getters.moreRecords(channel) },
-    [name ? name + 'NoRecords' : 'noRecords']() { return this.$store.getters.noRecords(channel) },
+    [name ? name : 'model']() {
+      return this.$store.getters.channel(dynamic ? this[channel] : channel)
+    },
+    [name ? name + 'Error' : 'error']() {
+      return this.$store.getters.error(dynamic ? this[channel] : channel)
+    },
+    [name ? name + 'Loading' : 'loading']() {
+      return this.$store.getters.loading(dynamic ? this[channel] : channel)
+    },
+    [name ? name + 'Meta' : 'meta']() {
+      return this.$store.getters.meta(dynamic ? this[channel] : channel)
+    },
+    [name ? name + 'MoreRecords' : 'moreRecords']() {
+      return this.$store.getters.moreRecords(dynamic ? this[channel] : channel)
+    },
+    [name ? name + 'NoRecords' : 'noRecords']() {
+      return this.$store.getters.noRecords(dynamic ? this[channel] : channel)
+    },
   };
 };
